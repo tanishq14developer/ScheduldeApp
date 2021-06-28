20dcdc3827a08d721821d65861b4321a99e7c7c6
tanishq14Developer <tanishq.happy@gmail.com>
2021-06-28 20:54:30 +0530

    second commit

--- app/src/main/java/com/example/task/Adapter/AdapterClass.java
+++ app/src/main/java/com/example/task/Adapter/AdapterClass.java
@@ -1,47 +1,28 @@
 package com.example.task.Adapter;
 
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import com.example.task.fragment.fragment_home;
+import com.smarteist.autoimageslider.SliderViewAdapter;
 
-import com.example.task.R;
-import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
+public class AdapterClass extends SliderViewAdapter {
 
-
-
-public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder> {
-
-    int a[];
-
-    public AdapterClass(int[] a) {
-        this.a = a;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home,parent,false));
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull  AdapterClass.MyViewHolder holder, int position) {
+    public AdapterClass(fragment_home fragment_home) {
 
     }
 
     @Override
-    public int getItemCount() {
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+
+    }
+
+    @Override
+    public int getCount() {
         return 0;
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        RelativeLayout layout;
-        public MyViewHolder(@NonNull  View itemView) {
-            super(itemView);
-            layout = itemView.findViewById(R.id.phase1);
-        }
     }
 }
--- app/src/main/java/com/example/task/fragment/fragment_home.java
+++ app/src/main/java/com/example/task/fragment/fragment_home.java
@@ -1,5 +1,6 @@
 package com.example.task.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,43 +9,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.task.Adapter.AdapterClass;
 import com.example.task.R;
-import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 
 
 public class fragment_home extends Fragment {
-    AdapterClass adapterClass;
-    WormDotsIndicator dotsIndicator;
-    ViewPager2 pager2;
-    int list[];
+    SliderView sliderView;
+    private AdapterClass aClass;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view =inflater.inflate(R.layout.fragment_home, container, false);
-       pager2  = view.findViewById(R.id.view_pager);
-       dotsIndicator = view.findViewById(R.id.dot1);
-       list = new int[3];
-       list[0] = R.id.rlone;
-       list[1] = R.id.rltwo;
-       list[2] = R.id.rlthree;
-       adapterClass = new AdapterClass(list);
-       pager2.setAdapter(adapterClass);
-       pager2.setClipToPadding(false);
-       pager2.setClipChildren(false);
-       pager2.setOffscreenPageLimit(3);
-        CompositePageTransformer transformer = new CompositePageTransformer();
-        transformer.addTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull  View page, float position) {
-                float a = 1-Math.abs(position);
-                page.setScaleY(0.85f + a*0.15f);
-            }
-        });
-        pager2.setPageTransformer(transformer);
+       sliderView = view.findViewById(R.id.imageslider);
+       aClass = new AdapterClass(fragment_home.this);
+       sliderView.setSliderAdapter(aClass);
+       sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+       sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+       sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+       sliderView.setIndicatorSelectedColor(Color.BLUE);
+       sliderView.setIndicatorUnselectedColor(Color.GRAY);
+       sliderView.setScrollTimeInSec(3);
+       sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+           @Override
+           public void onIndicatorClicked(int position) {
+
+           }
+       });
        return view;
     }
 
